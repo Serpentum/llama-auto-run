9a5977256a542743c3393a766aa1707ddb2abaa8
bogdan <bogdan@seotlt.ru>
2026-05-20 22:14:16 +0400

    feat: добавить --log-file '' для принудительного вывода логов в консоль

--- autoLLAma/app.py
+++ autoLLAma/app.py
@@ -34,6 +34,7 @@ DEFAULT_ARGS = [
     "--repeat-penalty", "1.0",
     "--reasoning-budget", "-1",
     "--chat-template-kwargs", '{"preserve_thinking": true}',
+    "--log-file", "",
     "--host", "0.0.0.0",
     "--port", "8033",
 ]
